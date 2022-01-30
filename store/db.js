86c7426611f4c0d583da2f2ae8837368759f9652
Gabriel Bourgeois <gabriel.bourgeoisv4si@gmail.com>
2022-01-30 00:36:09 -0500

    feat(store): add support for DATABASE_LOCATION env variable

--- store/db.js
+++ store/db.js
@@ -1,5 +1,7 @@
 const level = require('level')
 const uuidv4 = require('uuid').v4
 
-exports.db = level('./store/pickem-db')
+const location = process.env.DATABASE_LOCATION || './store/pickem-db'
+
+exports.db = level(location)
 exports.generateId = () => uuidv4()
